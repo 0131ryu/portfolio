04ec7559fc22bf6db589ba566d321cd45a8517c0
0131ryu <131ryuji@gmail.com>
2023-02-27 18:07:57 +0900

    next, typescript, tailwindss 적용

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "#6096B4",
+        "light-blue": "#93BFCF",
+        "gray-blue": "#BDCDD6",
+        "light-beige": "#EEE9DA",
+        "dark-red": "#AA5656",
+      },
+    },
+  },
+  plugins: [],
+};
